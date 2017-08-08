887d5c9c9b80ed977b1a66592c713b7c257a7f45
david rolfs <davidrolfs15@gmail.com>
2017-08-08 10:19:30 -0700

    add tests

---
+++ ToDoList.Tests/ItemTest.cs
@@ -0,0 +1,22 @@
+﻿using IdentityStuff.Models;
+using Xunit;
+
+namespace IdentityStuff.Tests
+{
+    public class ItemTest
+    {
+        [Fact]
+        public void GetDescriptionTest()
+        {
+            //Arrange
+            var item = new Item();
+            item.Description = "Wash the dog";
+
+            //Act
+            var result = item.Description;
+
+            //Assert
+            Assert.Equal("Wash the dog", result);
+        }
+    }
+}
